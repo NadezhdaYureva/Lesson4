f70520c425686f257810961cfa618ebce9aee1fa
nadezhdayu <nadezhdayureva1987@gmail.com>
2022-06-26 19:27:03 +0300

    Решение задачи 1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string number = Console.ReadLine();
+    int numInt = int.Parse(number);
+    return numInt;
+}
+
+int powerInt (int number, int power)
+{
+    if (power ==0) return 1;
+    int result = number;
+    for (int i = 1; i<power; i++)
+    {
+       result=result*number;
+    }
+    return result;
+}
+
+int a = Prompt("Введите число А ");
+int b = Prompt("Введите число B ");
+Console.WriteLine($"Число А в степени B равно {powerInt(a, b)}");
