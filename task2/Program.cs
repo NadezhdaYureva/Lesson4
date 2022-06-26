ef76ec562eb2e0dfc72d62a5d9c6f3029cdbf01e
nadezhdayu <nadezhdayureva1987@gmail.com>
2022-06-26 19:34:23 +0300

    Решение задачи 2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int prompt (string message)
+{
+    Console.Write (message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int SumDigits (int number)
+{
+    int digit = 0;
+    while (number > 0) 
+    {
+        digit=digit+number%10; 
+        number = number / 10; 
+    }
+    return digit;
+}
+
+int number = prompt("Введите число ");
+Console.WriteLine($"Сумма цифр в числе {number} равна {SumDigits(number)}");
